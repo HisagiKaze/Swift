967fcbcf1a820db6bdaff44265dcb1d6fea511e1
Paul-Aurian POINOT <paulaurianpoinot@macbook-pro-de-paul-aurian.local>
2017-05-12 01:05:53 +0200

    Ajout d'alarme, de couleurs etc

--- Minuteur/Minuteur/ViewController.swift
+++ Minuteur/Minuteur/ViewController.swift
@@ -21,30 +21,64 @@ class ViewController: UIViewController {
         compteur?.invalidate()
         i = 0
         compteurLabel.text = "00:00"
+        lecteur.stop()
+        compteurLabel.textColor = UIColor.black
+        jouerSon(son: "bip")
     }
     
     @IBAction func pauseAction(_ sender: UIBarButtonItem) {
         compteur?.invalidate()
+        lecteur.pause()
     }
     
     @IBAction func startTimerAction(_ sender: UIBarButtonItem) {
         compteur = Timer.scheduledTimer(timeInterval: 1, target: self, selector:
             #selector(ViewController.incrementerCompteur), userInfo: nil, repeats: true)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         compteur = Timer()
-        let fichierSon = Bundle.main.path(forResource: "bip", ofType: "mp3")
+        
+        jouerSon(son: "bip")
+    }
+    
+    func jouerSon(son:String) {
+        let fichierSon = Bundle.main.path(forResource: son, ofType: "mp3")
+        
+        do {
+            try lecteur = AVAudioPlayer(contentsOf: URL(string:fichierSon!)!)
+        }
+        catch {
+            print("erreur lecture son")
+        }
     }
     
     func incrementerCompteur() {
     
-        i += 1
+        let limite = 12
+        
+        if (i == limite) {
+            jouerSon(son : "alarm")
+            lecteur.play()
+            compteurLabel.textColor = UIColor.red
+        }
+        else if (limite - i < 3) {
+            compteurLabel.textColor = UIColor.orange
+            i += 1
+            lecteur.play()
+        }
+        else {
+            i += 1
+            lecteur.play()
+        }
+        
         compteurLabel.text = "\(i)"
         var timeStr:String?
         
+        //Formater minutes-Secondes
         let minutes = i / 60 % 60
         let secondes = i % 60
         
