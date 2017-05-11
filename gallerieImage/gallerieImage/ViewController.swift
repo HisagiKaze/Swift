5f1fd4955b82c24512922f2a63449795f94ed1d9
Paul-Aurian POINOT <paulaurianpoinot@macbook-pro-de-paul-aurian.local>
2017-05-11 23:14:10 +0200

    fin

--- gallerieImage/gallerieImage/ViewController.swift
+++ gallerieImage/gallerieImage/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images:[String] = ["lion.png", "canyon.png", "concert.png", "esb.png", "sommet.png", "voiture.png"]
+    var index = 1
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBAction func changerImageAction(_ sender: UIButton) {
+        
+        
+        if(index == images.count - 1) {
+            index = 0
+        }
+        else {
+            index = index + 1
+        }
+        let imageStr:String = images[index]
+        
+        image.image = UIImage(named: imageStr)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
